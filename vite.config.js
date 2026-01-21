4e9682ee91ea3349f2bc2fbdd515b1328df0c22e
veritozy <121317594+veritozy@users.noreply.github.com>
2026-01-21 09:06:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/proyectoZara/'
+  base: '/proyectoZara/',
   build: {
     rollupOptions: {
       input: {
